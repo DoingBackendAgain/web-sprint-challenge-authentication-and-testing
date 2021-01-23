762d96a22719f3ab56c28a147d6ceebc4fab44c4
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-23 17:00:40 -0500

    middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,46 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
+const jwt = require("jsonwebtoken")
+
+
+function restrict(){
+   return async (req, res, next) => {
+
+    try{
+
+      const token = req.cookies.token
+
+      if(!token){
+        return res.status(401).json({
+          message: "Token required"
+        })
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=> {
+        if(err){
+          return res.status(401).json({
+            message: "Token Invalid"
+          })
+        }
+
+        req.token = decoded 
+
+        next()
+      })
+
+
+    }
+    catch(err){
+      next(err)
+    }
+  
+   }
+}
+
+module.exports = {
+  restrict
+}
+
+
+/*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
@@ -11,4 +51,3 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
