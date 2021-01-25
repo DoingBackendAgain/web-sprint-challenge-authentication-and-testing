38e6095a86ff293c49e76f9b8c0f8bd15c895698
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-25 13:52:58 -0500

    test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,18 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const supertest = require("supertest")
+const server = require("./server.js")
+//const db = require("../data/dbConfig")
+
+
+// test('sanity', () => {
+//   expect(true).toBe(true)
+// })
+
+describe("registration test", () => {
+  it("posts to database", async ()=> {
+    const res = await supertest(server)
+      .post("register")
+      .send({username: "Steph", password: "abc123"})
+      expect(res.statusCode).toBe(201)
+  })
 })
